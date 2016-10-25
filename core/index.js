f2804d4f4aa131a837199d5f4f09fa04cc81e5ea
aplgreenh <aplgreenh@gmail.com>
2016-10-25 10:25:57 +0900

    Add server debug function

--- core/index.js
+++ core/index.js
@@ -1,3 +1,44 @@
+Object.defineProperty(global, '__stack', {
+get: function() {
+        var orig = Error.prepareStackTrace;
+        Error.prepareStackTrace = function(_, stack) {
+            return stack;
+        };
+        var err = new Error;
+        Error.captureStackTrace(err, arguments.callee);
+        var stack = err.stack;
+        Error.prepareStackTrace = orig;
+        return stack;
+    }
+});
+
+Object.defineProperty(global, '__dbg', {
+    get: function() {
+        var msg =  __stack[1].getFileName().replace('/Users/dev/Ghost/','/') + '::' + __stack[1].getFunctionName() + '()';
+        console.log(msg);
+        return msg;
+    }
+});
+
+Object.defineProperty(global, '__dbga', {
+    get: function() {
+        return function(rsvargs) {
+            var msg =  __stack[1].getFileName().replace('/Users/dev/Ghost/','/') + '::' + __stack[1].getFunctionName();
+            if(arguments.length > 0)
+            {
+                msg +=  '(' + JSON.stringify(rsvargs) + ')';
+            }
+            else
+            {
+                msg +=  '()';
+            }
+
+            console.log(msg);
+            return msg;
+        };
+    }
+});
+
 // ## Server Loader
 // Passes options through the boot process to get a server instance back
 var server = require('./server');
